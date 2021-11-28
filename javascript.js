63eaa102a50a16c132260bea0c368bedaceb458a
nikmakhija <90490335+nikmakhija@users.noreply.github.com>
2021-11-28 18:56:03 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,4 @@
+document.getElementById("button1").style.resize="vertical"
+ducument.getElementById("button2").style.color="blue"
+document.getElementById("button3").style.color="fade"
+document.getElementById("button4").style.resize="original"
